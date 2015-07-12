735572a70fd626b3c063d900fce084120f7ac3f3
OHTSUKA Ko-hei <kochizufan@gmail.com>
2015-07-12 14:07:14 +0900

    タイルツイカ

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 (function(){
     var map = L.map('map').setView([35.0, 135.0], 5);
+    L.tileLayer('http://j.tile.openstreetmap.jp/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; OpenStreetMap contributors, Tiles Courtesy of OpenStreetMap Japan',
+        maxZoom: 19,
+    }).addTo(map);
 })();
